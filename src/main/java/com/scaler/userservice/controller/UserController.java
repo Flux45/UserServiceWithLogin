5a6e090b02900891866630bd4e2a382ef2e4e08f
Ayush <aayyuusshh.45@gmail.com>
2024-04-03 17:19:46 +0530

    Made Basic Project Structure

---
+++ src/main/java/com/scaler/userservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.scaler.userservice.controller;
+
+import com.scaler.userservice.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    public User login() {
+        // check if email and password in db
+        // if yes return user
+        // else throw some error
+        return null;
+    }
+    public User signUp() {
+        // no need to hash password for now
+        // just store user as is in the db
+        // for now no need to have email verification either
+        return null;
+    }
+
+    public ResponseEntity<Void> logout() {
+        // delete token if exists -> 200
+        // if doesn't exist give a 404
+        return null;
+    }
+}
---
+++ src/main/java/com/scaler/userservice/models/BaseModel.java
@@ -0,0 +1,14 @@
+package com.scaler.userservice.models;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseModel {
+    @Id
+    private Long id;
+}
