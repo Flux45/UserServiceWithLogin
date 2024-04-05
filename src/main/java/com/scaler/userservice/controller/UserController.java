2b4280785337e2325e91e81f69375bd63e9bd562
Ayush <aayyuusshh.45@gmail.com>
2024-04-05 20:46:46 +0530

    Added Login, Logout and Validate

--- src/main/java/com/scaler/userservice/controller/UserController.java
+++ src/main/java/com/scaler/userservice/controller/UserController.java
@@ -1,29 +1,69 @@
 package com.scaler.userservice.controller;
 
+import com.scaler.userservice.Repository.TokenRepository;
+import com.scaler.userservice.dtos.LoginRequestDto;
+import com.scaler.userservice.dtos.LogoutRequestDto;
+import com.scaler.userservice.dtos.SignUpRequestDto;
+import com.scaler.userservice.exception.UserNotFountException;
+import com.scaler.userservice.models.Token;
 import com.scaler.userservice.models.User;
+import com.scaler.userservice.service.UserService;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    public User login() {
+    private UserService userService;
+    private RestTemplate restTemplate;
+    private final TokenRepository tokenRepository;
+
+    @Autowired
+    public UserController(@Qualifier("SelfUserService") UserService userService,RestTemplate restTemplate,
+                          TokenRepository tokenRepository) {
+        this.userService = userService;
+        this.restTemplate = restTemplate;
+        this.tokenRepository = tokenRepository;
+    }
+
+    @PostMapping("/login")
+    public Token login(@RequestBody LoginRequestDto loginRequestDto) throws UserNotFountException {
         // check if email and password in db
         // if yes return user
         // else throw some error
-        return null;
+        return userService.login(loginRequestDto.getEmail(),loginRequestDto.getPassword());
     }
-    public User signUp() {
+
+    @PostMapping("/signup")
+    public User signUp(@RequestBody SignUpRequestDto requestDto) {
         // no need to hash password for now
         // just store user as is in the db
         // for now no need to have email verification either
-        return null;
+        String email = requestDto.getEmail();
+        String password = requestDto.getPassword();
+        String name = requestDto.getName();
+        return userService.signUp(name,email,password);
     }
 
-    public ResponseEntity<Void> logout() {
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestBody LogoutRequestDto requestDto) {
         // delete token if exists -> 200
         // if doesn't exist give a 404
-        return null;
+
+        userService.logout(requestDto.getToken());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/validate/{token}")
+    public User validateToken(@PathVariable("token") @NotNull String token) {
+        return userService.validateToken(token);
     }
 }
